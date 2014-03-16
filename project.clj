bc42dd0cf707df7c76a828a4006c4ce38f395b8d
Silvan Spross <silvan.spross@gmail.com>
2014-03-16 16:37:55 +0100

    more project config

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject startingclojure "0.1.0-SNAPSHOT"
+(defproject startingclojure "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.2"]
-                 [compojure "1.1.6"]])
+                 [compojure "1.1.6"]]
+  :uberjar-name "startingclojure-standalone.jar"
+  :min-lein-version "2.0.0")
