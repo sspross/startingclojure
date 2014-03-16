7fc94acce0b7d90cebed5653fc26226b65f00e9d
Silvan Spross <silvan.spross@gmail.com>
2014-03-16 16:31:44 +0100

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/startingclojure-standalone.jar clojure.main -m startingclojure.app $PORT
---
+++ src/startingclojure/app.clj
@@ -0,0 +1,38 @@
+(ns startingclojure.app
+  (:use (compojure handler
+                   [core :only (GET POST defroutes)]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
+
+(def counter (atom 10000))
+
+(def urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)
+    id))
+
+(defn home
+  [request]
+  (str @urls))
+
+(defn redirect
+  [id]
+  (response/redirect (@urls id)))
+
+;;(defroutes app
+;;  (GET "/" [request] (home request))
+;;  (GET "/:id" [id] (redirect id)))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port) :join? false}))
+
+;;(def server (jetty/run-jetty #'app {:port 8080 :join? false}))
