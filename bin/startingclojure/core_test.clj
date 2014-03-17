83f1488a556d36704f20a170f9ffc555674c60b9
Silvan Spross <silvan.spross@gmail.com>
2014-03-17 14:21:38 +0100

    extended gitignore and removed unneeded files

--- bin/startingclojure/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns startingclojure.core-test
-  (:require [clojure.test :refer :all]
-            [startingclojure.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
